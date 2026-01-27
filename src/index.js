393fff0f97c092e06c093c9ece2cc7c5cc3e3f03
sauravsirswal-stack <saurav.sirswal@chicmicstudios.in>
2026-01-27 17:34:33 +0530

    server setup and sequelize setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 const express = require("express")
+const {PORT} = require("./config/serverConfig")
+
+
+const setupAndStartServer = async()=>{
+    const app = express()
+
+    app.use(express.json())
+    app.use(express.urlencoded({extended : true}))
+
+
+    app.listen(PORT, ()=>{
+        console.log(`server listening on port ${PORT}`);
+        
+    })
+}
+
+setupAndStartServer()
